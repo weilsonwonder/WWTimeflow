9e57e29bdbbc5be27ae1355c78339b31645c9b62
Weilson Wonder <weilson@live.com>
2016-09-14 14:47:10 +0800

    Convert to Swift 3

--- Sources/WWTimeflow.swift
+++ Sources/WWTimeflow.swift
@@ -1,111 +1,111 @@
 import Foundation
 
 public class WWTimeflow {
-    public static var calendar: NSCalendar = {
-        let cal = NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian)!
+    public static var calendar: Calendar = {
+        var cal = Calendar(identifier: Calendar.Identifier.gregorian)
         cal.timeZone = timeZoneUTC
         return cal
     }()
-    public static var timeZone: NSTimeZone {
+    public static var timeZone: TimeZone {
         set { calendar.timeZone = newValue }
         get { return calendar.timeZone }
     }
-    public static var dateFormatter: NSDateFormatter {
-        let df = NSDateFormatter()
+    public static var dateFormatter: DateFormatter {
+        let df = DateFormatter()
         df.timeZone = timeZone
         df.calendar = calendar
         return df
     }
-    private static let timeZoneUTC: NSTimeZone = NSTimeZone(abbreviation: "UTC")!
+    private static let timeZoneUTC: TimeZone = TimeZone(abbreviation: "UTC")!
     
     public private(set) var value: Int
-    public private(set) var unit: NSCalendarUnit
+    public private(set) var unit: Calendar.Component
     
-    private init(value: Int, unit: NSCalendarUnit) {
+    init(value: Int, unit: Calendar.Component) {
         self.value = value
         self.unit = unit
     }
 }
 
-extension NSDate: Comparable {}
+//extension Date: Comparable {}
 
-public func == (lhs: NSDate, rhs: NSDate) -> Bool {
-    return lhs.compare(rhs) == .OrderedSame
+public func == (lhs: Date, rhs: Date) -> Bool {
+    return lhs.compare(rhs) == .orderedSame
 }
 
-public func < (lhs: NSDate, rhs: NSDate) -> Bool {
-    return lhs.compare(rhs) == .OrderedAscending
+public func < (lhs: Date, rhs: Date) -> Bool {
+    return lhs.compare(rhs) == .orderedAscending
 }
 
-public func > (lhs: NSDate, rhs: NSDate) -> Bool {
-    return lhs.compare(rhs) == .OrderedDescending
+public func > (lhs: Date, rhs: Date) -> Bool {
+    return lhs.compare(rhs) == .orderedDescending
 }
 
-public func <= (lhs: NSDate, rhs: NSDate) -> Bool {
-    return lhs.compare(rhs) != .OrderedDescending
+public func <= (lhs: Date, rhs: Date) -> Bool {
+    return lhs.compare(rhs) != .orderedDescending
 }
 
-public func >= (lhs: NSDate, rhs: NSDate) -> Bool {
-    return lhs.compare(rhs) != .OrderedAscending
+public func >= (lhs: Date, rhs: Date) -> Bool {
+    return lhs.compare(rhs) != .orderedAscending
 }
 
-public func - (lhs: NSDate, rhs: NSDate) -> NSTimeInterval {
-    return lhs.timeIntervalSinceDate(rhs)
+public func - (lhs: Date, rhs: Date) -> TimeInterval {
+    return lhs.timeIntervalSince(rhs)
 }
 
-public func + (lhs: NSDate, rhs: WWTimeflow) -> NSDate {
-    return WWTimeflow.calendar.dateByAddingUnit(rhs.unit, value: rhs.value, toDate: lhs, options: .SearchBackwards)!
+public func + (lhs: Date, rhs: WWTimeflow) -> Date {
+    return WWTimeflow.calendar.date(byAdding: rhs.unit, value: rhs.value, to: lhs)!
 }
 
-public func - (lhs: NSDate, rhs: WWTimeflow) -> NSDate {
-    return WWTimeflow.calendar.dateByAddingUnit(rhs.unit, value: -rhs.value, toDate: lhs, options: .SearchBackwards)!
+public func - (lhs: Date, rhs: WWTimeflow) -> Date {
+    return WWTimeflow.calendar.date(byAdding: rhs.unit, value: -rhs.value, to: lhs)!
 }
 
 public extension Int {
     public var year: WWTimeflow {
-        return WWTimeflow(value: self, unit: .Year)
+        return WWTimeflow(value: self, unit: .year)
     }
     public var years: WWTimeflow {
         return year
     }
     
     public var month: WWTimeflow {
-        return WWTimeflow(value: self, unit: .Month)
+        return WWTimeflow(value: self, unit: .month)
     }
     public var months: WWTimeflow {
         return month
     }
     
     public var week: WWTimeflow {
-        return WWTimeflow(value: self, unit: .WeekOfYear)
+        return WWTimeflow(value: self, unit: .weekOfYear)
     }
     public var weeks: WWTimeflow {
         return week
     }
     
     public var day: WWTimeflow {
-        return WWTimeflow(value: self, unit: .Day)
+        return WWTimeflow(value: self, unit: .day)
     }
     public var days: WWTimeflow {
         return day
     }
     
     public var hour: WWTimeflow {
-        return WWTimeflow(value: self, unit: .Hour)
+        return WWTimeflow(value: self, unit: .hour)
     }
     public var hours: WWTimeflow {
         return hour
     }
     
     public var minute: WWTimeflow {
-        return WWTimeflow(value: self, unit: .Minute)
+        return WWTimeflow(value: self, unit: .minute)
     }
     public var minutes: WWTimeflow {
         return minute
     }
     
     public var second: WWTimeflow {
-        return WWTimeflow(value: self, unit: .Second)
+        return WWTimeflow(value: self, unit: .second)
     }
     public var seconds: WWTimeflow {
         return second
@@ -131,114 +131,114 @@ public extension Int {
 }
 
 public extension String {
-    public func dateFromFormat(format: String) -> NSDate? {
+    public func dateFromFormat(format: String) -> Date? {
         let formatter = WWTimeflow.dateFormatter
         formatter.dateFormat = format
-        return formatter.dateFromString(self)
+        return formatter.date(from: self)
     }
 }
 
-public extension NSDate {
+public extension Date {
     public func stringFromFormat(format: String) -> String {
         let formatter = WWTimeflow.dateFormatter
         formatter.dateFormat = format
-        return formatter.stringFromDate(self)
+        return formatter.string(from: self)
     }
     
-    public func differenceWith(date: NSDate, inUnit unit: NSCalendarUnit) -> Int {
-        return WWTimeflow.calendar.components(unit, fromDate: self, toDate: date, options: .SearchBackwards).valueForComponent(unit)
+    public func differenceWith(date: Date, inUnit unit: Calendar.Component) -> Int {
+        return WWTimeflow.calendar.dateComponents([unit], from: self, to: date).value(for: unit)!
     }
     
-    public func components(units: NSCalendarUnit) -> NSDateComponents {
-        return WWTimeflow.calendar.components(units, fromDate: self)
+    public func components(units: Set<Calendar.Component>) -> DateComponents {
+        return WWTimeflow.calendar.dateComponents(units, from: self)
     }
     
-    public convenience init(year: Int, month: Int, day: Int, hour: Int, minute: Int, second: Int) {
-        let d = NSDate().change(year: year, month: month, day: day, hour: hour, minute: minute, second: second)
+    public init(year: Int, month: Int, day: Int, hour: Int, minute: Int, second: Int) {
+        let d = Date().change(year: year, month: month, day: day, hour: hour, minute: minute, second: second)
         self.init(timeIntervalSinceReferenceDate: d.timeIntervalSinceReferenceDate)
     }
     
-    public convenience init(year: Int, month: Int, day: Int) {
-        let d = NSDate().change(year: year, month: month, day: day, hour: 0, minute: 0, second: 0)
+    public init(year: Int, month: Int, day: Int) {
+        let d = Date().change(year: year, month: month, day: day, hour: 0, minute: 0, second: 0)
         self.init(timeIntervalSinceReferenceDate: d.timeIntervalSinceReferenceDate)
     }
     
-    public func change(year year: Int? = nil, month: Int? = nil, day: Int? = nil, hour: Int? = nil, minute: Int? = nil, second: Int? = nil) -> NSDate {
-        let components = self.components([.Year, .Month, .Day, .Hour, .Minute, .Second])
+    public func change(year: Int? = nil, month: Int? = nil, day: Int? = nil, hour: Int? = nil, minute: Int? = nil, second: Int? = nil) -> Date {
+        var components = self.components(units: [.year, .month, .day, .hour, .minute, .second])
         components.year = year ?? self.year
         components.month = month ?? self.month
         components.day = day ?? self.day
         components.hour = hour ?? self.hour
         components.minute = minute ?? self.minute
         components.second = second ?? self.second
-        return WWTimeflow.calendar.dateFromComponents(components)!
+        return WWTimeflow.calendar.date(from: components)!
     }
     
-    public func change(weekday weekday: Int) -> NSDate {
+    public func change(weekday: Int) -> Date {
         return self - (self.weekday - weekday).days
     }
     
     public var year: Int {
-        return components(.Year).year
+        return components(units: [.year]).year!
     }
     public var month: Int {
-        return components(.Month).month
+        return components(units: [.month]).month!
     }
     public var day: Int {
-        return components(.Day).day
+        return components(units: [.day]).day!
     }
     public var hour: Int {
-        return components(.Hour).hour
+        return components(units: [.hour]).hour!
     }
     public var minute: Int {
-        return components(.Minute).minute
+        return components(units: [.minute]).minute!
     }
     public var second: Int {
-        return components(.Second).second
+        return components(units: [.second]).second!
     }
     public var weekday: Int {
-        return components(.Weekday).weekday
+        return components(units: [.weekday]).weekday!
     }
     
-    public var beginningOfYear: NSDate {
+    public var beginningOfYear: Date {
         return change(month: 1, day: 1, hour: 0, minute: 0, second: 0)
     }
-    public var endOfYear: NSDate {
-        return (beginningOfYear + 1.year).dateByAddingTimeInterval(-1)
+    public var endOfYear: Date {
+        return (beginningOfYear + 1.year).addingTimeInterval(-1)
     }
     
-    public var beginningOfMonth: NSDate {
+    public var beginningOfMonth: Date {
         return change(day: 1, hour: 0, minute: 0, second: 0)
     }
-    public var endOfMonth: NSDate {
-        return (beginningOfMonth + 1.month).dateByAddingTimeInterval(-1)
+    public var endOfMonth: Date {
+        return (beginningOfMonth + 1.month).addingTimeInterval(-1)
     }
     
-    public var beginningOfWeek: NSDate {
+    public var beginningOfWeek: Date {
         return change(weekday: 1).beginningOfDay
     }
-    public var endOfWeek: NSDate {
-        return (beginningOfWeek + 1.week).dateByAddingTimeInterval(-1)
+    public var endOfWeek: Date {
+        return (beginningOfWeek + 1.week).addingTimeInterval(-1)
     }
     
-    public var beginningOfDay: NSDate {
+    public var beginningOfDay: Date {
         return change(hour: 0, minute: 0, second: 0)
     }
-    public var endOfDay: NSDate {
-        return (beginningOfDay + 1.day).dateByAddingTimeInterval(-1)
+    public var endOfDay: Date {
+        return (beginningOfDay + 1.day).addingTimeInterval(-1)
     }
     
-    public var beginningOfHour: NSDate {
+    public var beginningOfHour: Date {
         return change(minute: 0, second: 0)
     }
-    public var endOfHour: NSDate {
-        return (beginningOfHour + 1.hour).dateByAddingTimeInterval(-1)
+    public var endOfHour: Date {
+        return (beginningOfHour + 1.hour).addingTimeInterval(-1)
     }
     
-    public var beginningOfMinute: NSDate {
+    public var beginningOfMinute: Date {
         return change(second: 0)
     }
-    public var endOfMinute: NSDate {
-        return (beginningOfMinute + 1.minute).dateByAddingTimeInterval(-1)
+    public var endOfMinute: Date {
+        return (beginningOfMinute + 1.minute).addingTimeInterval(-1)
     }
 }
--- WWTimeflow.podspec
+++ WWTimeflow.podspec
@@ -1,13 +1,13 @@
 Pod::Spec.new do |s|
   s.name         = "WWTimeflow"
-  s.version      = "1.0.2"
+  s.version      = "2.0.0"
   s.summary      = "NSDate easy manipulator for comparison and changing dates."
   s.homepage     = "https://github.com/weilsonwonder/WWTimeflow"
   s.license      = { :type => "MIT", :file => "LICENSE" }
   s.author             = { "Weilson Wonder" => "weilson@live.com" }
   s.social_media_url   = "https://sg.linkedin.com/in/weilson"
 
-  s.ios.deployment_target = "8.0"
+  s.ios.deployment_target = "9.0"
 
   s.source       = { :git => "https://github.com/weilsonwonder/WWTimeflow.git", :tag => s.version }
   s.source_files  = "Sources/*.swift"
